fa1b9d58ce6f20ba17f465b92bb6d2bbbb68e728
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2024-12-18 00:01:54 +0100

    feat: add imperfect solution to task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+    let encryptedMessage = '';
+    let messageUppercase = message.toUpperCase();
+    let keyUppercase = key.toUpperCase();
+    let keyIndex = 0;
+
+    for (let i = 0; i < messageUppercase.length; i += 1) {
+      const letter = messageUppercase[i];
+      if (alphabet.includes(letter)) {
+        const letterIndex = alphabet.indexOf(letter);
+        const shiftIndex = alphabet.indexOf(keyUppercase[keyIndex % keyUppercase.length]);
+        const newIndex = (letterIndex + shiftIndex) % alphabet.length;
+        encryptedMessage += alphabet[newIndex];
+        keyIndex += 1;
+      } else {
+        encryptedMessage += letter;
+      }
+    }
+
+    return encryptedMessage;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+    let decryptedMessage = '';
+    let encryptedMessageUppercase = encryptedMessage.toUpperCase();
+    let keyUppercase = key.toUpperCase();
+    let keyIndex = 0;
+
+    for (let i = 0; i < encryptedMessageUppercase.length; i += 1) {
+      const letter = encryptedMessageUppercase[i];
+      if (alphabet.includes(letter)) {
+        const letterIndex = alphabet.indexOf(letter);
+        const shiftIndex = alphabet.indexOf(keyUppercase[keyIndex % keyUppercase.length]);
+        const newIndex = (letterIndex - shiftIndex + alphabet.length) % alphabet.length;
+        decryptedMessage += alphabet[newIndex];
+        keyIndex += 1;
+      } else {
+        decryptedMessage += letter;
+      }
+    }
+
+    return decryptedMessage;
   }
 }
 
